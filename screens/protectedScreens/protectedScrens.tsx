53a8fbce5e286b7cec513fa076a9b3a93375dd16
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-17 00:48:52 +0100

    feat: tab navigation created for protected screens

--- screens/protectedScreens/protectedScrens.tsx
+++ screens/protectedScreens/protectedScrens.tsx
@@ -1,6 +1,15 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View } from "react-native";
+import ProductScreen from "./ProductScreen";
+import CartScreen from "./CartScreen";
 
+const Tab = createBottomTabNavigator();
 const ProtectedScreen = () => {
-  return <View></View>;
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Product" component={ProductScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+    </Tab.Navigator>
+  );
 };
 export default ProtectedScreen;
