2ddb7605924e4d226d88ef78af4a863d78753e49
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-17 01:09:04 +0100

    feat: product action done

--- apis/action/product-action.ts
+++ apis/action/product-action.ts
@@ -1,1 +1,13 @@
-export const useProtuctAction = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { productService } from "../service/product-service";
+
+export const useProductAction = () => {
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await productService();
+      console.log(res.data);
+      return res.data;
+    },
+  });
+};
--- screens/protectedScreens/ProductScreen.tsx
+++ screens/protectedScreens/ProductScreen.tsx
@@ -1,6 +1,8 @@
 import { View } from "react-native";
 
 const ProductScreen = () => {
+  //  const {res} = usePro
+
   return <View></View>;
 };
 export default ProductScreen;
