5145892de3c3de6edc79f2ddff54f16b7a09bc5e
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-17 16:49:06 +0100

    feat: product data displayed

--- apis/action/product-action.ts
+++ apis/action/product-action.ts
@@ -8,7 +8,7 @@ export const useProductAction = () => {
     queryFn: async () => {
       const res = await productService();
       //   console.log(res.data);
-      return res.data;
+      return res.data.products;
     },
   });
 };
--- screens/protectedScreens/ProductScreen.tsx
+++ screens/protectedScreens/ProductScreen.tsx
@@ -1,23 +1,44 @@
-import { FlatList, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import { useProductAction } from "../../apis/action/product-action";
 import { useEffect } from "react";
 
 const ProductScreen = () => {
-  const { data } = useProductAction();
+  const { data, isLoading, error } = useProductAction();
+  // useEffect(() => {
+  //   if (data) {
+  //     data;
+  //   }
+  // }, []);
+
   useEffect(() => {
-    if (data) {
-      console.log("product", data);
-    }
-  }, []);
+    console.log("Products data:", data);
+    console.log("Loading:", isLoading);
+    console.log("Error:", error);
+  }, [data, isLoading, error]);
 
   return (
-    <View>
-      <FlatList
-        data={data}
-        renderItem={(item) => {
-          return <View></View>;
-        }}
-      />
+    <View style={{ flex: 1, padding: 20, borderWidth: 2, borderColor: "red" }}>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => {
+            console.log(item);
+
+            return (
+              <View>
+                <Image
+                  source={{ uri: item.image }}
+                  style={{ width: 300, height: 200 }}
+                />
+                <Text>{item.price}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item._id as string}
+        />
+      )}
     </View>
   );
 };
