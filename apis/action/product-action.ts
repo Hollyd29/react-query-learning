75bba66f3388ca317d6ed866721ca6e9a1a7a3b9
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-18 17:51:54 +0100

    feat: add to cart action done

--- apis/action/product-action.ts
+++ apis/action/product-action.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { productService } from "../service/product-service";
+import { addToCartService, productService } from "../service/product-service";
 import { DataProp } from "../../utils/types/data.type";
+import Toast from "react-native-toast-message";
 
 export const useProductAction = () => {
   return useQuery<any, Error, DataProp[]>({
@@ -15,6 +16,20 @@ export const useProductAction = () => {
 
 export const useAddToCartAction = () => {
   return useMutation({
-    mutationFn: async (id) => {},
+    mutationFn: async (id: string) => {
+      const res = await addToCartService(id);
+      console.log(res.data);
+      return res.data;
+    },
+    onSuccess: () => {
+      Toast.show({
+        type: "error",
+        text1: "Product add to cart successful",
+        visibilityTime: 3000,
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
   });
 };
