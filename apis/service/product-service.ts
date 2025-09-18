71c05bf483a834c7d086a189c402f285a4067657
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-18 17:28:24 +0100

    feat: add to car service done

--- apis/service/product-service.ts
+++ apis/service/product-service.ts
@@ -3,3 +3,7 @@ import api from "../api";
 export const productService = async () => {
   return await api.get("/products");
 };
+
+export const addToCartService = async () => {
+  return await api.post("/addToCart");
+};
