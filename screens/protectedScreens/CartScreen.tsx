9fe2ca8548d7438d1482a3002d87c0adf03d8b34
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-20 16:35:48 +0100

    feat: remove all cart added

--- screens/protectedScreens/CartScreen.tsx
+++ screens/protectedScreens/CartScreen.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Image, Text, View } from "react-native";
+import { Button, FlatList, Image, Text, View } from "react-native";
 import { useAllCartAction } from "../../apis/action/product-action";
 import { useEffect } from "react";
 // import { getAuthToken, removeAuthToken } from "../../utils/storage/token";
@@ -23,20 +23,23 @@ const CartScreen = () => {
       {isLoading ? (
         <Text>Loading...</Text>
       ) : (
-        <FlatList
-          data={data}
-          renderItem={(each) => {
-            return (
-              <View>
-                <Image
-                  source={{ uri: each.item.image }}
-                  style={{ width: 300, height: 200 }}
-                />
-                <Text>{each.item.price}</Text>
-              </View>
-            );
-          }}
-        />
+        <View>
+          <Button title="Remove all cart" />
+          <FlatList
+            data={data}
+            renderItem={(each) => {
+              return (
+                <View>
+                  <Image
+                    source={{ uri: each.item.image }}
+                    style={{ width: 300, height: 200 }}
+                  />
+                  <Text>{each.item.price}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
       )}
     </View>
   );
