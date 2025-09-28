a44c605e43e6ca5af8e3a2e0d06519d3dddf3ae2
Hollyd29 <olapejuoladimeji30@gmail.com>
2025-09-28 20:16:15 +0100

    feat: example for click ripple with color and active button done

--- screens/protectedScreens/ButtonComp.tsx
+++ screens/protectedScreens/ButtonComp.tsx
@@ -1,6 +1,53 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 const ButtonComp = () => {
-  return <View></View>;
+  const [active, setActive] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>();
+
+  const activate = (name: string) => {
+    setName(name);
+    // setActive(false);
+    setActive(true);
+  };
+  return (
+    <View>
+      <Pressable
+        onPress={() => activate("submit")}
+        style={({ pressed }) => [
+          {
+            marginBlock: 20,
+            marginHorizontal: 20,
+            padding: 10,
+            backgroundColor: pressed
+              ? "green"
+              : active && name === "submit"
+              ? "#ccc"
+              : "white",
+          },
+        ]}
+      >
+        <Text>Submit</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => activate("enter")}
+        style={({ pressed }) => [
+          {
+            marginBlock: 20,
+            marginHorizontal: 20,
+            padding: 10,
+            backgroundColor: pressed
+              ? "green"
+              : active && name === "enter"
+              ? "#ccc"
+              : "white",
+          },
+        ]}
+      >
+        <Text>Enter</Text>
+      </Pressable>
+    </View>
+  );
 };
 export default ButtonComp;
